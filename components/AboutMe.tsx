25a61ca4152b7a35b8791b3b47946c279998202b
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-10-02 14:50:34 -0400

    Update

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -35,12 +35,10 @@ export default function AboutMe() {
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        <span className="italic">When I'm not coding</span>, I enjoy fitness, reading, and walking my dog. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="font-medium">sales and marketing.</span>
       </p>
     </motion.section>
   );
